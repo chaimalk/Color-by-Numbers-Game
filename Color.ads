fc99525ec310d3634b24b1ad2335a0a6193e25ee
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 02:09:40 +0100

    Create Color.ads

---
+++ Color.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/0011074865",
+    "admobInterstitial": "ca-app-pub-1010770342437944/0032584845",
+    "applovinBanner": "db247ecead46e0bf",
+    "applovinInterstitial": "9549be08895e4361",
+    "unityGameId": "5172251",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
